be9a5c735dc861e9d7ad424da591c7dba38ba09d
lhuang <lhuang@cnexlabs.com>
2019-01-16 16:51:51 +0800

    initial files

---
+++ README.md
@@ -0,0 +1,85 @@
+# Introduction
+
+一个基于`python3.6`和`Django2.0`的博客。 
+
+A simple blog based on `python3.6` and `Django2.0`.
+
+## requirement
+- Django==2.0.3
+- django-jet==1.0.7
+- django-summernote==0.8.8.6
+- pytz==2018.3
+  
+## Front page
+
+![1](http://jinbitou.net/wp-content/uploads/2018/03/2018031504572726.png)
+
+
+![2](http://jinbitou.net/wp-content/uploads/2018/03/2018031504572826.png)
+
+---
+
+![3](http://jinbitou.net/wp-content/uploads/2018/03/2018031504572475.png)
+
+
+---
+
+## Backend page
+![1](http://jinbitou.net/wp-content/uploads/2018/03/2018031504572953.png)
+
+---
+
+![2](http://jinbitou.net/wp-content/uploads/2018/03/2018031504574323.png)
+
+---
+![3](http://jinbitou.net/wp-content/uploads/2018/03/201803091037415.png)
+
+---
+
+![4](http://jinbitou.net/wp-content/uploads/2018/03/201803091037396.png)
+
+---
+
+## Completed
+
+#### CN
+- 文章管理，包括新增、删除和编辑
+- 分类管理，包括新增、删除和编辑
+- 标签管理，包括新增、删除和编辑
+- 列表页展示概要信息、发布时间、分类、浏览次数
+- 分页展示
+- 点击"阅读全文"显示文章详细内容
+- 详情页每刷新一次浏览次数+1
+- 文章分类搜索
+- 文章标签搜索
+- 文章详细页切换上一篇和下一篇文章
+- 点击图标回到顶部
+- 文章按月归档
+
+#### EN
+- Article management, including adding, deleting and editing
+- Classified management, including adding, deleting and editing
+- Tag management, including adding, deleting and editing
+- List page shows summary information, time of release, category, number of visits
+- Pagination display
+- Click "read the full text" to display the details of the article
+- Pages plus one per view
+- Search by category
+- Search by tag
+- switch between previous and next articles
+- One-click back to the top
+- Monthly archive
+
+## TO-DO
+
+#### CN
+
+- 关键字搜索
+- 评论
+- 标签云
+
+#### EN
+
+- Keyword search
+- Comments
+- Tag Cloud
---
+++ jbt_blog/urls.py
@@ -0,0 +1,48 @@
+"""jbt_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, reverse_lazy
+from apps.blog import views
+from django.conf.urls import include
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib.auth import views as contrib_auth_views
+
+urlpatterns = [
+    path(r'admin/', admin.site.urls),
+    path(r'', views.home, name='home'),
+    path(r'home/', views.home, name='home'),
+    path(r'accounts/login/', contrib_auth_views.login,{'template_name':'login.html', 'extra_context':{'next':'/'}}, name='login'),
+    path(r'accounts/logout/', contrib_auth_views.LogoutView.as_view(next_page=reverse_lazy('login')),
+        name='logout'),
+    path(r'articles/<int:id>/', views.detail, name='detail'),
+    path(r'category/<int:id>/', views.search_category, name='category_menu'),
+    path(r'tag/<str:tag>/', views.search_tag, name='search_tag'),
+    path(r'archives/<str:year>/<str:month>', views.archives, name='archives'),
+    path(r'summernote/', include('django_summernote.urls')),
+    path(r'jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path(r'jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG})
+    ]

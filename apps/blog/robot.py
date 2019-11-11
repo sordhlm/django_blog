7a92ff6bc30a51c84722c54403cef23d3338fd75
lhuang <lhuang@cnexlabs.com>
2019-11-11 15:02:54 +0800

    delete personal information for werobot

--- apps/blog/robot.py
+++ apps/blog/robot.py
@@ -7,90 +7,16 @@ from apps.tensorflow_poems.gen_good_poems import PoemPool
 from apps.blog.models import User, Poem
 
 robot = WeRoBot(enable_session=False,
-                token='ilovemuyao',
-                APP_ID='wx1bad78267ac404dc',
-                APP_SECRET='fb8162c70f72ddb2c1ceeff0d35e3567')
+                token='',
+                APP_ID='',
+                APP_SECRET='')
 poem = PoemPool()
 vena = venaAI()
-#client = robot.client
-#client.create_menu({
-#    "button":[
-#        {
-#            "type": "click",
-#            "name": u"茶室外话",
-#            "key" : "click_chat"
-#        },
-#        {
-#            "name": '草庐弄诗',
-#            "sub_button":[
-#                {
-#                    "type": "click",
-#                    "name": "类型",
-#                    "key" : "poem_ptype"
-#                },
-#                {
-#                    "type": "click",
-#                    "name": "初始",
-#                    "key" : "poem_init"
-#                },
-#                {
-#                    "type": "click",
-#                    "name": "成诗",
-#                    "key" : "poem_create"
-#                },
-#                {
-#                    "type": "click",
-#                    "name": "问",
-#                    "key" : "poem_query"
-#                },
-#
-#            ]
-#        }]
-#})
-#
+
 @robot.subscribe
 def subscribe(message):
     User.objects.create(source=message.source)
     return '欢迎关注本公众号！'
-#
-#@robot.key_click("poem_ptype")
-#def poem_init(message):
-#    user = User.objects.get(source=message.source)
-#    user.mode = 2
-#    user.save()
-#    ret = '请回复数字选择诗的类型: 0) 五绝  1）七绝'
-#    return ret
-#
-#@robot.key_click("poem_init")
-#def poem_init(message):
-#    user = User.objects.get(source=message.source)
-#    user.mode = 3
-#    user.save()
-#    ret = '请输入你的底稿'
-#    return ret
-#
-#@robot.key_click("poem_create")
-#def poem_create(message):
-#    user = User.objects.get(source=message.source)
-#    poem.add(user.source, user.pinit, user.ptype, user.thrd)
-#    ret = poem.gen(message.content)
-#    return ret
-#
-#@robot.key_click("poem_query")
-#def poem_query(message):
-#    ret = poem.get_result(message.source)
-#    if ret:
-#        res = ret[0]
-#    else:
-#        res = "poems is on-generating"
-#    return ret
-#
-#@robot.key_click("click_chat")
-#def click_chat(message):
-#    user = User.objects.get(source=message.source)
-#    user.mode = 1
-#    user.save()
-#    return "let's chat"
 
 @robot.text
 def text_handle(message):
